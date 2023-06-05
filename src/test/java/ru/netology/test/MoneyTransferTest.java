5b10523dc871aa7babf9dd940e402c2d2fc38f3c
Igor Rubanov <fomagreen@yandex.ru>
2023-06-05 19:45:02 +0300

    modify test

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,26 +1,43 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
+import ru.netology.page.DashBoardPage;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
 
 public class MoneyTransferTest {
-        @Test
-    void testTransaction () {
-            open("http://localhost:9999");
-            var loginPage = new LoginPage();
-            var authInfo = DataHelper.getAuthInfo();
-            var verificationPage = loginPage.validLogin(authInfo);
-            var verificationCode = DataHelper.getVerificationCode();
-            var dashboardPage = verificationPage.validVerify(verificationCode);
-            var transactionPage = dashboardPage.fromFirstToSecondCard();
-            var transactionInfo = DataHelper.getFirstCard();
-            transactionPage.inputTransaction(transactionInfo, "5000");
-            var expected = 15_000;
-            var actual = dashboardPage.getCardBalance(1);
-            assertEquals(expected, actual);
-        }
+    LoginPage loginPage;
+    DashBoardPage dashBoardPage;
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashBoardPage = verificationPage.validVerify(verificationCode);
+
+    }
+
+    @Test
+    void testTransaction() {
+        var firstCardBalance = dashBoardPage.getCardBalance(0);
+        var secondCardBalance = dashBoardPage.getCardBalance(1);
+        var amount = 5000;
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transactionPage = dashBoardPage.fromFirstToSecondCard();
+        var transactionInfo = DataHelper.getFirstCard();
+        dashBoardPage = transactionPage.inputTransaction(transactionInfo, String.valueOf(amount));
+
+        var actualBalanceFirstCard = dashBoardPage.getCardBalance(0);
+        var actualBalanceSecondCard = dashBoardPage.getCardBalance(1);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
 }
